fe1529228a41b56a8ef94d16100e6c87224faa52
heehee812 <e.enyuu812@gmail.com>
2021-06-27 01:54:02 +0800

    Add check icon

--- components/element/ThreeDots.tsx
+++ components/element/ThreeDots.tsx
@@ -5,13 +5,21 @@ import Icon from 'react-native-vector-icons/MaterialIcons'
 const ICON_SIZE = 24
 
 import React, { useState, useEffect } from 'react'
-import { View } from 'react-native';
+import { View, Switch, StyleSheet} from 'react-native';
 import { Button, Menu, Divider, Provider } from 'react-native-paper';
 
 const ThreeDots = () => {
+  //menu
   const [visible, setVisible] = useState(false);
   const openMenu = () => setVisible(true);
   const closeMenu = () => setVisible(false);
+  //toggle
+  const [isEnabled, setIsEnabled] = useState(false);
+  const toggleChecked = () => setIsEnabled(previousState => !previousState);
+  const toggleDay = () => setIsEnabled(previousState => !previousState);
+  const toggleWeek = () => setIsEnabled(previousState => !previousState);
+  const toggleMonth = () => setIsEnabled(previousState => !previousState);
+
 
   return (
     <Provider>
@@ -25,17 +33,61 @@ const ThreeDots = () => {
             color={'grey'}
             onPress={openMenu}/>
           }>
-          <Menu.Item onPress={() => {}} title="checked" />
+          <View style={{flexDirection: 'row', flexGrow: 1, justifyContent:'center', alignItems: 'center'}}>
+            <Menu.Item onPress={() => {}} title="checked" />
+            <Switch
+            trackColor={{ false: "#767577", true: "#81b0ff" }}
+            thumbColor={isEnabled ? "#f5dd4b" : "#f4f3f4"}
+            ios_backgroundColor="#3e3e3e"
+            onValueChange={toggleChecked}
+            value={isEnabled}
+            />
+          </View>
           <Divider />
-          <Menu.Item onPress={() => {}} title="day" />
+          <View style={{flexDirection: 'row', flexGrow: 1, justifyContent:'center', alignItems: 'center'}}>
+            <Menu.Item onPress={() => {}} title="day" />
+            <Switch
+            trackColor={{ false: "#767577", true: "#81b0ff" }}
+            thumbColor={isEnabled ? "#f5dd4b" : "#f4f3f4"}
+            ios_backgroundColor="#3e3e3e"
+            onValueChange={toggleDay}
+            value={isEnabled}
+            />
+          </View>
           <Divider />
-          <Menu.Item onPress={() => {}} title="week" />
-          <Divider />
-          <Menu.Item onPress={() => {}} title="month" />
+          <View style={{flexDirection: 'row', flexGrow: 1, justifyContent:'center', alignItems: 'center'}}>
+            <Menu.Item onPress={() => {}} title="week" />
+            <Switch
+            trackColor={{ false: "#767577", true: "#81b0ff" }}
+            thumbColor={isEnabled ? "#f5dd4b" : "#f4f3f4"}
+            ios_backgroundColor="#3e3e3e"
+            onValueChange={toggleWeek}
+            value={isEnabled}
+            />
+          </View>
+            <Divider />
+          <View style={{flexDirection: 'row', flexGrow: 1, justifyContent:'center', alignItems: 'center'}}>
+            <Menu.Item onPress={() => {}} title="month" />
+            <Switch
+            trackColor={{ false: "#767577", true: "#81b0ff" }}
+            thumbColor={isEnabled ? "#f5dd4b" : "#f4f3f4"}
+            ios_backgroundColor="#3e3e3e"
+            onValueChange={toggleMonth}
+            value={isEnabled}
+            />
+          </View>
         </Menu>
       </View>
     </Provider>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "center"
+  }
+});
 
 export default ThreeDots;
