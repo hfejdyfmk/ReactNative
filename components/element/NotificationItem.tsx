c71619ac5b4acf789d4726c72b9e82dcc1ff99b9
heehee812 <e.enyuu812@gmail.com>
2021-06-27 11:12:41 +0800

    Button checked function finish

--- components/element/NotificationItem.tsx
+++ components/element/NotificationItem.tsx
@@ -5,12 +5,18 @@ import { connect } from 'react-redux';
 import {View, Text} from '../Themed'
 import { GiftedChat, InputToolbar } from 'react-native-gifted-chat'
 import Icon from 'react-native-vector-icons/MaterialIcons'
+import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons'
 import { StyleSheet } from 'react-native';
 
 const ICON_SIZE = 24;
 
 const NotificationItem = () => {
   const [messages, setMessages] = useState<any[]>([]);
+  const [visible, setVisible] = useState(false);
+  const openMenu = () => setVisible(true);
+  const closeMenu = () => setVisible(false);
+  const [isCheckedEnabled, setCheckedIsEnabled] = useState(false);
+  const setCheckedIcon = () => setCheckedIsEnabled(previousState => !previousState);
 
   useEffect(() => {
     setMessages([
@@ -35,10 +41,12 @@ const NotificationItem = () => {
         minInputToolbarHeight={0}
         renderInputToolbar={() => null}
       />
-      <Icon
-          name='check-circle'
+      <MaterialCommunityIcons.Button
+          name={isCheckedEnabled? 'check-circle': 'check-circle-outline'}
           color={'grey'}
           size={ICON_SIZE}
+          onPress={setCheckedIcon}
+          backgroundColor={'transparent'}
       />
       <Text
         style={{color: 'grey'}}
